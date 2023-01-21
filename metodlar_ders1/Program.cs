fec4ee19c30993898f33c501e1c24a4f29485a4d
Feytoo <90255881+Feytoo@users.noreply.github.com>
2023-01-22 01:44:55 +0300

    Add files via upload

---
+++ metodlar_ders1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace metodlar_ders1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //erisim_belirteci geri_donustipi metot_adi(parametreListesi/arguman)
+        //{
+            //komutlar;
+            //return;
+        //}
+
+        int a = 2;
+        int b = 3;
+        Console.WriteLine(a+b);
+
+        int sonuc = Topla(a,b);
+        Console.WriteLine(sonuc);
+
+        Metotlar ornek = new Metotlar();
+        ornek.ekranaYazdir(Convert.ToString(sonuc));
+
+        int sonuc2 = ornek.artirveTopla(ref a,ref b);
+        ornek.ekranaYazdir(Convert.ToString(sonuc2));
+        ornek.ekranaYazdir(Convert.ToString(a+b));
+    }
+
+    static int Topla(int deger1, int deger2)
+    {
+        return(deger1+deger2);
+    }
+}
+class Metotlar
+{
+    public void ekranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public int artirveTopla(ref int deger1,ref int deger2)
+    {
+        deger1 +=1;
+        deger2 +=1;
+        return deger1+deger2;
+    }
+}
