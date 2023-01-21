f1b1865288bc1805fb64cf54e8fcd8c454ea98fc
Feytoo <90255881+Feytoo@users.noreply.github.com>
2023-01-22 01:19:49 +0300

    Add files via upload

---
+++ switch_case/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace switch_case;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int month = DateTime.Now.Month;
+        
+        switch (month)
+        {
+            case 1:
+                Console.WriteLine("Ocak Ayindasiniz!");
+                break;
+            case 3:
+                Console.WriteLine("Mart Ayindasiniz!");
+                break;
+            case 4:
+                Console.WriteLine("Nisan Ayindasiniz!");
+                break;
+            case 2:
+                Console.WriteLine("Subat Ayindasiniz!");
+                break;
+            case 5:
+                Console.WriteLine("Mayis Ayindasiniz!");
+                break;
+            default:
+            break;
+        }
+        switch (month)
+        {
+            case 12:
+            case 1:
+            case 2:
+                Console.WriteLine("Kis Mevsimindesiniz!");
+                break;
+
+            case 3:
+            case 4:
+            case 5:
+                Console.WriteLine("Ilkbahar Mevsimindesiniz!");
+                break; 
+
+            case 6:
+            case 7:
+            case 8:
+                Console.WriteLine("Yaz Mevsimindesiniz!");
+                break;
+
+            case 9:
+            case 10:
+            case 11:
+                Console.WriteLine("Sonbahar Mevsimindesiniz!");
+                break;
+                 
+            default:
+            break;
+        }
+    }
+}
---
+++ while_foreach/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace while_foreach;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //while
+        // 1 den baslayarak console dan girilen sayiya kadar olan sayilarin ortalama heaplayip console a yazdirma.
+        Console.Write("Sayi giriniz: ");
+        int sayi = int.Parse(Console.ReadLine());
+        int sayac= 1, toplam=0;
+        while(sayac <= sayi)
+        {
+            toplam += sayac;
+            sayac++;
+        }
+        Console.WriteLine("Ortalama: "+(toplam/sayi));
+
+        // a dan z ye kadar butun alfabeyi yazdirma
+        char character = 'a';
+        while(character <= 'z')
+        {
+            Console.Write(character);
+            character++;
+        }
+        //Foreach
+        Console.WriteLine("***** Foreach *****");
+        string[] arabalar = {"BMW","Ford","Nissan","Toyota"};
+
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);            
+        }
+    }
+}
