36d2e16381ae28e5eff0c53d32d0cd1296f23e42
Feytoo <90255881+Feytoo@users.noreply.github.com>
2023-01-22 22:05:52 +0300

    Add files via upload

---
+++ metodlar_ders2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace metodlar_ders2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //out parametreler
+        string sayi = "999";
+        int outSayi;
+
+        bool sonuc = int.TryParse(sayi, out outSayi);
+        if(sonuc)
+        {
+            Console.WriteLine("Basarili!");
+            Console.WriteLine(outSayi);
+        }
+        else
+        {
+            Console.WriteLine("Basarisiz!");
+        }
+
+        Metotlar instance = new Metotlar();
+        instance.Topla(4,5,out int toplamSonucu);
+        Console.WriteLine(toplamSonucu);
+
+        // metod asiri yukleme - overloading
+        int ifade = 999;
+        instance.EkranaYazdir(ifade);
+        instance.EkranaYazdir("Fatih","Calik");
+    }
+}
+class Metotlar
+{
+    public void Topla(int a, int b, out int toplam)
+    {
+        toplam = a+b;
+    }
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(int veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(string veri1, string veri2)
+    {
+        Console.WriteLine(veri1+" "+ veri2);
+    }
+}
