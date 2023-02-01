a9e9b23af58ae350c3c4fd538a487148432b536a
Feytoo <90255881+Feytoo@users.noreply.github.com>
2023-02-01 03:47:30 +0300

    Add files via upload

---
+++ static/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace static_sinif;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calisan sayisi: {0}",Calisan.CalisanSayisi);
+
+        Calisan calisan1 = new Calisan("Ayse","Yilmaz","IK");
+        Console.WriteLine("Calisan sayisi: {0}",Calisan.CalisanSayisi);
+
+        Console.WriteLine("Toplama isleminin sonucu: {0}",Islemler.Toplam(100,200));
+        Console.WriteLine("Cikarma isleminin sonucu: {0}",Islemler.Cikarma(540,210));
+
+    }
+}
+
+class Calisan
+{
+    private static int calisanSayisi;
+
+    public static int CalisanSayisi {get => calisanSayisi;}
+
+    private string Isim;
+    private string Soyisim;
+    private string Departman;
+
+    static Calisan()
+    {
+        calisanSayisi = 0;
+    }
+    public Calisan(string isim, string soyisim, string departman)
+    {
+        this.Isim = isim;
+        this.Soyisim = soyisim;
+        this.Departman = departman;
+        calisanSayisi++;
+    }
+}
+
+static class Islemler
+{
+    public static long Toplam(int sayi1,int sayi2)
+    {
+        return sayi1 + sayi2;
+    } 
+    public static long Cikarma(int sayi1,int sayi2)
+    {
+        return sayi1 - sayi2;
+    }
+}
