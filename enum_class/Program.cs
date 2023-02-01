fc2c2732ca3df35dfc35387fb178e3db122c6ec9
Feytoo <90255881+Feytoo@users.noreply.github.com>
2023-02-01 04:10:46 +0300

    Add files via upload

---
+++ enum_class/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace enum_class;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Gunler.Pazar);
+        Console.WriteLine((int)Gunler.Cumartesi);
+
+        int sicaklik =32;
+
+        if(sicaklik < (int)HavaDurumu.Normal)
+            Console.WriteLine("Diariya cikmak icin havanin biraz isinmasini bekleyelim.");
+        else if (sicaklik > (int)HavaDurumu.Sicak)
+            Console.WriteLine("Disariya cikmak icin sicak bir gun.");
+        else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.CokSicak)
+            Console.WriteLine("Hadi disari cikalim.");
+    }
+}
+
+enum Gunler
+{
+    Pazartesi=1,
+    Sali,
+    Carsamba,
+    Persembe,
+    Cuma=23,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu
+{
+    Soguk = 5,
+    Normal = 20,
+    Sicak = 25,
+    CokSicak =30
+}
